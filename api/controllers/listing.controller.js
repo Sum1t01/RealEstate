9623f951f8c8f031676ef7d1d0b43700ba6cb2c3
Sumit Gupta <sg065387@gmail.com>
2024-04-18 17:22:56 +0545

    Created listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,17 +1,35 @@
-import express from "express";
 import Listing from "../models/listing.model.js";
+import errorHandler from "../utils/error.js";
 
-const createListing = async (req, res, next)=>{
-    try
-    {
+const createListing = async (req, res, next) => {
+    try {
         const listing = await Listing.create(req.body);
-        
+
         return res.status(201).json(listing);
     }
-    catch(error)
-    {
+    catch (error) {
         next(error);
     }
 };
 
-export {createListing};
+const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, "Listnig not found"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "Unauthorized access"));
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Listing deleted" });
+    }
+    catch (error) {
+        next(error);
+    }
+};
+
+export { createListing, deleteListing };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -175,6 +175,28 @@ function Profile() {
     }
   };
 
+  const handleListingDelete = async (id) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${id}`, {
+        method: "DELETE"
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings(
+        (prev)=>prev.filter((listing)=>listing._id!==id));
+
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -217,35 +239,35 @@ function Profile() {
 
       {userListings && userListings.length > 0 &&
         <div className='flex flex-col gap-3'>
-<h1 className='text-center my-7 text-2xl font-semibold'>
-  Your Listings
-</h1>
+          <h1 className='text-center my-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
           {
-          userListings.map((listing) => (
-            <div key={listing._id} className='border p-3 rounded-lg flex justify-between items-center gap-4 hover:shadow-lg' >
-              <Link to={`/listing/${listing._id}`}>
-                <img src={listing.imageURLs[0]} alt="Listing Cover" className='h-16 w-16 object-contain' />
+            userListings.map((listing) => (
+              <div key={listing._id} className='border p-3 rounded-lg flex justify-between items-center gap-4 hover:shadow-lg' >
+                <Link to={`/listing/${listing._id}`}>
+                  <img src={listing.imageURLs[0]} alt="Listing Cover" className='h-16 w-16 object-contain' />
 
-              </Link>
+                </Link>
 
-              <Link to={`/listing/${listing._id}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate ">
-                <p >
-                  {listing.name}
-                </p>
-              </Link>
+                <Link to={`/listing/${listing._id}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate ">
+                  <p >
+                    {listing.name}
+                  </p>
+                </Link>
 
-              <div className='flex flex-col gap-2'>
-                <button type="button" className='text-red-700 uppercase'>
-                  Delete
+                <div className='flex flex-col gap-2'>
+                  <button onClick={() => handleListingDelete(listing._id)} type="button" className='text-red-700 uppercase'>
+                    Delete
 
-                </button>
-                <button type="button" className='text-green-700 uppercase'>
-                  Edit
+                  </button>
+                  <button type="button" className='text-green-700 uppercase'>
+                    Edit
 
-                </button>
+                  </button>
+                </div>
               </div>
-            </div>
-          ))
+            ))
           }
 
         </div>
