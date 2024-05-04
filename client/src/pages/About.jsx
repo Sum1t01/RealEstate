5ffd8655cd2ccbbe6994eb24e4ee227f8a9d5be0
Sumit Gupta <sg065387@gmail.com>
2024-05-04 09:06:33 +0545

    Completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 
 function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-5 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About RealEstate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Welcome to RealEstate, your premier destination for finding your dream property! 
+        With an extensive database of listings across the globe, we're dedicated to helping
+         you discover the perfect home, whether you're searching for a cozy urban apartment,
+          a sprawling countryside estate, or a beachfront retreat.
+      </p>
+
+      <p className='mb-4 text-slate-700'>
+      Our user-friendly interface allows you to effortlessly browse through thousands of listings,
+       each accompanied by detailed descriptions, high-quality photos, and virtual tours, so you 
+       can explore every corner of your potential new home from the comfort of your current one.
+      </p>
+      
+      <p className='mb-4 text-slate-700'>
+      At RealEstate, we understand that buying or renting a property is more than just a transaction; 
+      it's a life-changing decision. That's why our team of experienced real estate professionals is 
+      here to guide you every step of the way, providing expert advice and personalized assistance to 
+      ensure a smooth and successful experience.
+      </p>
+
+      <p className='mb-4 text-slate-700'>
+      Whether you're a first-time buyer, a seasoned investor, or simply searching for your next adventure, 
+      RealEstate is your trusted partner in finding the perfect place to call home. Start your journey today 
+      and turn your real estate dreams into reality with RealEstate.
+      </p>
+    </div>
   );
 }
 
